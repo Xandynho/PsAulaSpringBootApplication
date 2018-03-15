c76a9314a44930c8ecc656f8ae5104bebb7aa8e0
“Xandynho” <x4ndynh065@hotmail.com>
2018-03-15 10:33:27 -0300

    Primeiro commit

---
+++ src/main/java/com/aulapablo/p5Spring/PsAulaSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.aulapablo.p5Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsAulaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsAulaSpringBootApplication.class, args);
+	}
+}
