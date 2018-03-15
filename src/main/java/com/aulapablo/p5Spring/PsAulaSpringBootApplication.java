13c04475a99ef053284c250f3c61fd47ce9d89a9
Xandynho <X4ndynh065@hotmail.com>
2018-03-15 11:28:12 -0300

    Segundo commit

--- src/main/java/com/aulapablo/p5Spring/PsAulaSpringBootApplication.java
+++ src/main/java/com/aulapablo/p5Spring/PsAulaSpringBootApplication.java
@@ -3,10 +3,13 @@ package com.aulapablo.p5Spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.aulapablo.p5Spring.resource.CategoriaResource;
+
 @SpringBootApplication
 public class PsAulaSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PsAulaSpringBootApplication.class, args);
+		
 	}
 }
